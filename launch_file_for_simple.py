9a7f7d5c04a1a0a70fa1aa518f605eb8364195a6
anirudh6415 <46961684+anirudh6415@users.noreply.github.com>
2019-06-10 01:05:50 +0530

    Create launch_file_for_simple.py

---
+++ launch_file_for_simple.py
@@ -0,0 +1,5 @@
+<launch>
+	<!-- beginner_tutorials launch file -->
+	<node pkg="beginner_tutorials" type="simple.py" name="ObiWan" output="screen"> #outputs on the screen (the output of simple.py)
+	</node>
+</launch>
